975f1f4d3331164e491068f69e799a7a0d518e38
ismailreformed <49227543+ismailreformed@users.noreply.github.com>
2019-12-25 19:20:18 +0600

    completed header for theme2

--- layouts/themeTwoLayout.vue
+++ layouts/themeTwoLayout.vue
@@ -7,84 +7,85 @@
     >
 
     <div style="background-color: white">
-      <v-container grid-list-xs class="hidden-xs-and-down">
-        <v-layout row wrap>
+      <v-container grid-list-xs class="my-0 pt-0 pb-0">
+        <v-layout  class="mt-4" row wrap>
           <v-flex lg1 xs3>
-            <v-avatar size="90">
-              <img
-                src="@/assets/resume_image.jpg"
-                alt="Ismail"
-              >
-            </v-avatar>
-            <v-btn class="my-2 hidden-sm-and-down" small rounded color="primary">
-              tap to chat
-            </v-btn>
+            <img
+            width="100"
+            height="100"
+              src="@/assets/resume_image.jpg"
+              alt="Ismail"
+            >
           </v-flex>
 
-          <v-flex lg2 xs7 class="mt-3">
-            <div class="ml-2">
-              <h3>
+          <v-flex lg4 xs7 class="ml-3">
+            <div class="ml-2" >
+              <h3 style="color: #0A56E9">
                 Ismail Hossain
               </h3>
-              <h6 class="overline font-weight-black">
-                Software Engineer
-              </h6>
-              <h6 class="caption">
+              <h6 style="color: #2E81F4" class="caption">
                 Laravel & Vue.js Developer
               </h6>
-           
+              <v-avatar size="40" v-for="item in listIcon" :key="item.id" class="listDesign my-2 mr-2">
+                <v-icon color="#0A56E9"> {{ item.icon }}</v-icon>
+              </v-avatar>
             </div>
           </v-flex>
-          <v-app-bar-nav-icon class="hidden-sm-and-up" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-          <v-flex lg9 class="hidden-sm-and-down mt-2">
-            <v-card-title class="mt-2 pt-0 ">
+          <v-flex xs1 class="hidden-sm-and-up pa-0 mt-5">
+            <v-icon color="#0A56E9">
+              mdi-text-to-speech-off
+            </v-icon> 
+            <br>
+            <v-icon color="#0A56E9" class="mt-3">
+              mdi-youtube-tv
+            </v-icon>
+          </v-flex>
+          <v-spacer></v-spacer>
+          <v-flex lg6 xs12>
+            <v-card-title class="my-0 py-0 hidden-sm-and-down">
               <div
                 class="ma-2"
               >
-                <div class="text-center body-1">
-                  View interviews:
-                </div>
+                <h3 class="text-center font-weight-bold body-1"  style="color: #0A56E9">
+                  View interviews :
+                </h3>
               </div>
-              <v-spacer />
-              <v-btn class="ma-2 text-capitalize" small outlined rounded color="primary">
-                <v-icon left>
+              <v-btn class="ma-2" color="#EEF6FF" style="box-shadow: none">
+                <v-icon left color="#0A56E9">
                   mdi-text-to-speech-off
-                </v-icon>  Audio & Text
+                </v-icon> 
+                <span style="color: #0A56E9">Audio & Text </span> 
               </v-btn>
-              <v-spacer />
-              <v-btn class="ma-2 text-capitalize" small rounded outlined color="primary">
-                <v-icon left>
+              <v-btn class="ma-2 " color="#EEF6FF" style="box-shadow: none">
+                <v-icon color="#0A56E9" left>
                   mdi-youtube-tv
-                </v-icon> Video
+                </v-icon> 
+                <span style="color: #0A56E9">Video </span>      
               </v-btn>
-              <v-spacer />
-
-              <div
-                class="ma-2"
-              >
-                <div class="text-center body-2 font-weight-bold">
-                  $15
+              </v-card-title>
+              <v-card-title class="ma-0 pb-1 pt-1 px-0">
+                <div
+                  class="ma-2"
+                >
+                  <div class="text-center body-2">
+                    <span style="font-size: x-large; color: #0A56E9">15$</span> 
+                    <br class="hidden-sm-and-up">
+                    <span style="color: #2E81F4"> Hourly Rate</span>
+                  </div>
                 </div>
-                <div class="text-center body-2 font-weight-bold">
-                  Hourly Rate
+                <div
+                  class="ma-2"
+                >
+                  <div class="text-center body-2">
+                    <span style="font-size: x-large; color: #0A56E9">10hr</span>
+                    <br class="hidden-sm-and-up">
+                    <span style="color: #2E81F4"> Weekly Availabilty</span>
+                  </div>
                 </div>
-              </div>
-              <v-spacer />
-              <div
-                class="ma-2"
-              >
-                <div class="text-center body-2 font-weight-bold">
-                  10 hours
-                </div>
-                <div class="text-center body-2 font-weight-bold">
-                  Weekly Availabilty
-                </div>
-              </div>
-              <v-spacer />
-              <v-btn rounded color="primary">
-                <span class="text-capitalize">Hire Me</span>
-              </v-btn>
-            </v-card-title>
+                <v-btn style="box-shadow: none" color="primary">
+                  <span class="text-capitalize">Hire Me</span>
+                </v-btn>
+              </v-card-title>
           </v-flex>
         </v-layout>
       </v-container>
@@ -111,133 +112,6 @@
         <nuxt />
       </v-container>
     </div>
-     <v-navigation-drawer
-     v-if="drawer"
-      v-model="drawer"
-      app
-      right
-    >
-      <v-list
-        nav
-        dense
-      >
-        <v-list-item-group
-          v-model="group"
-          active-class="deep-purple--text text--accent-4"
-        >
-          <v-list-item>
-            <v-list-item-title>
-              <v-layout>
-                <v-flex xs5>
-                  <v-avatar size="80">
-                    <img
-                      src="@/assets/resume_image.jpg"
-                      alt="Ismail"
-                    >
-                  </v-avatar>
-                </v-flex>
-                <v-flex xs7 class="mt-3">
-                  <span class="body-1 font-weight-black">
-                    Ismail Hossain
-                  </span>
-                  <br>
-                  <v-btn class="my-2" small rounded color="primary">
-                    tap to chat
-                  </v-btn>
-                </v-flex>
-              </v-layout>
-            </v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-
-        <v-subheader>
-         View Interviews
-        </v-subheader>
-
-        <v-list-item-group
-          v-model="group"
-          active-class="deep-purple--text text--accent-4"
-        >
-          <v-list-item>
-             <v-list-tile-avatar>
-              <v-icon left color="primary">
-                mdi-text-to-speech-off
-              </v-icon> 
-            </v-list-tile-avatar>
-
-            <v-list-item-title>
-              <v-btn class=" text-capitalize" small block outlined rounded color="primary">
-                 Audio & Text
-              </v-btn>
-            </v-list-item-title>
-          </v-list-item>
-          <v-list-item>
-             <v-list-tile-avatar>
-              <v-icon left color="primary">
-                mdi-youtube-tv
-              </v-icon>
-            </v-list-tile-avatar>
-
-            <v-list-item-title>
-              <v-btn class=" text-capitalize" small block outlined rounded color="primary">
-                 Video
-              </v-btn>
-            </v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-
-        <v-divider ></v-divider>
-
-        <v-list-item-group
-          v-model="group"
-          active-class="deep-purple--text text--accent-4"
-        >
-          <v-list-item>
-            <v-list-item-title>
-              <div
-                class="ma-2"
-              >
-                <div class="text-center body-2 font-weight-bold">
-                  $15
-                </div>
-                <div class="text-center body-2 font-weight-bold">
-                  Hourly Rate
-                </div>
-              </div>
-            </v-list-item-title>
-          </v-list-item>
-          <v-divider ></v-divider>
-          <v-list-item>
-            <v-list-item-title>
-              <div
-                class="ma-2"
-              >
-                <div class="text-center body-2 font-weight-bold">
-                  10 hours
-                </div>
-                <div class="text-center body-2 font-weight-bold">
-                  Weekly Availabilty
-                </div>
-              </div>
-            </v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-        <v-divider ></v-divider>
-        <v-list-item-group
-          v-model="group"
-          active-class="deep-purple--text text--accent-4"
-        >
-          <v-list-item>
-            <v-list-item-title>
-              <v-btn class="my-2" rounded block color="primary">
-                <span class="text-capitalize">Hire Me</span>
-              </v-btn>
-            </v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-        <v-divider ></v-divider>
-      </v-list>
-    </v-navigation-drawer>
     <Footer />
     </v-app>
   </v-card>
@@ -262,6 +136,12 @@ export default {
       ],
       drawer: false,
       group: null,
+      listIcon:[
+        { id: 'a', icon: 'mdi-message-text'},
+        { id: 'b', icon: 'mdi-behance'},
+        { id: 'c', icon: 'mdi-web'},
+        { id: 'd', icon: 'mdi-instagram'},
+      ]
     }
   },
    watch: {
@@ -282,4 +162,19 @@ export default {
   max-width: 300px;
 }
 
+ul {
+  list-style-type: none;
+}
+
+li{
+  display: inline-block
+}
+
+
+.listDesign{
+  height: 40px;
+  width: 40px;
+  border: 2px solid #0A56E9;
+  background-color: #EEF6FF
+}
 </style>
